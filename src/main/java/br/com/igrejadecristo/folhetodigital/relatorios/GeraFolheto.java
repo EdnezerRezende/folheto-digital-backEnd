12e87be9426537e1c71486ab00c46f9a1f8cd637
ednezerrezende <godoirezende@gmail.com>
2020-02-14 14:56:02 -0300

    Alteração de Configuracao de email permitindo o controle pela classe EmailConfig

--- src/main/java/br/com/igrejadecristo/folhetodigital/relatorios/GeraFolheto.java
+++ src/main/java/br/com/igrejadecristo/folhetodigital/relatorios/GeraFolheto.java
@@ -5,48 +5,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.igrejadecristo.folhetodigital.entidades.Mensagem;
+import br.com.igrejadecristo.folhetodigital.respositories.IgrejaRepository;
 import br.com.igrejadecristo.folhetodigital.respositories.MensagemRepository;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.view.JasperViewer;
 
 public class GeraFolheto {
 	@Autowired
 	private MensagemRepository mensagemDAO;
 	
+	@Autowired
+	private IgrejaRepository igrejaDAO;
+	
 	public void gerar(String layout) throws JRException, SQLException, ClassNotFoundException {
 
 		// gerando o jasper design
-		JasperDesign desenho = JRXmlLoader.load(layout);
+//		JasperDesign desenho = JRXmlLoader.load(layout);
 
-		// compila o relatório
-		JasperReport relatorio = JasperCompileManager.compileReport(desenho);
+//		// compila o relatório
+//		JasperReport relatorio = JasperCompileManager.compileReport(desenho);
 
-		// estabelece conexão
-//		Class.forName(driver);
-//		Connection con = DriverManager.getConnection(url, login, pwd);
-//		Statement stm = con.createStatement();
-//		String query = "select * from turma";
-//		ResultSet rs = stm.executeQuery(query);
-//
 //		// implementação da interface JRDataSource para DataSource ResultSet
 //		JRResultSetDataSource jrRS = new JRResultSetDataSource(rs);
-		//Captura os dados
-		List<Mensagem> mensagens = mensagemDAO.findAllByOrderByDataCriado();
 		
+		List<Mensagem> mensagens = mensagemDAO.findAllByOrderByDataCriado();
+		JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(mensagens);
 		// executa o relatório
-		Map parametros = new HashMap();
-//		parametros.put("nota", new Double(10));
-		parametros.put("mensagem", mensagens.get(0));
-		JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros);
+		Map parametros = new HashMap(); // Usar parametros para informar dados mais gerenciais do relatório, tipo a data .... 
+		parametros.put("dataFolhetim", LocalDate.now());
+		parametros.put("mensagem", mensagens.get(0).getMensagem());
+		parametros.put("mensagemAutor", mensagens.get(0).getAutor());
+		parametros.put("mensagemTitulo", mensagens.get(0).getTitulo());
+		JasperPrint impressao = JasperFillManager.fillReport(layout, parametros, ds);
 
 		// exibe o resultado
 		JasperViewer viewer = new JasperViewer(impressao, true);
@@ -55,7 +52,7 @@ public class GeraFolheto {
 
 	public static void main(String[] args) {
 		try {
-			new GeraFolheto().gerar("report.jrxml");
+			new GeraFolheto().gerar("./resources/relatorios/report.jrxml");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
