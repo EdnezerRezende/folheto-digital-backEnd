d2ea89b3a2c8d434e2ad203980ec7cda06878550
ednezer <godoirezende@gmail.com>
2020-03-23 21:30:35 -0300

    Criando folhetim via jasper

--- src/main/java/br/com/igrejadecristo/folhetodigital/relatorios/GeraFolheto.java
+++ src/main/java/br/com/igrejadecristo/folhetodigital/relatorios/GeraFolheto.java
@@ -2,6 +2,8 @@ package br.com.igrejadecristo.folhetodigital.relatorios;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,7 @@ import org.springframework.core.io.ResourceLoader;
 
 import br.com.igrejadecristo.folhetodigital.respositories.IgrejaRepository;
 import br.com.igrejadecristo.folhetodigital.respositories.MensagemRepository;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -45,14 +48,21 @@ public class GeraFolheto {
 		//JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(mensagens);
 		// executa o relatório
 		Map parametros = new HashMap(); // Usar parametros para informar dados mais gerenciais do relatório, tipo a data .... 
-		//parametros.put("dataFolhetim", LocalDate.now());
-		//parametros.put("mensagem", mensagens.get(0).getMensagem());
+		parametros.put("dataFolhetim", "07/03/2020");
+		parametros.put("enderecoIgreja", "QSB 10/11 A.E. 09 CEP: 72015-600 Taguatinga Sul-DF");
+		parametros.put("telefoneIgreja", "(61) 3563-1865");
+		parametros.put("siteIgreja", "www.ictaguatinga.com.br");
+		parametros.put("emailIgreja", "cristeltagsul@bol.com.br");
+		parametros.put("mensagem", "texto de mensagem gerado pelo jasper");
+		parametros.put("tituloMensagem", "Quase no Reino de Deus?");
+		parametros.put("autorMensagem", "Pr Gerson Vicente de Sousa");
+//		parametros.put("mensagem", mensagens.get(0).getMensagem());
 		//parametros.put("mensagemAutor", mensagens.get(0).getAutor());
 		//parametros.put("mensagemTitulo", mensagens.get(0).getTitulo());
 		JasperReport jr;
         JasperPrint jprint;
 		jr = JasperCompileManager.compileReport(resource.getURL().getPath());
-		JasperPrint impressao = JasperFillManager.fillReport(jr, parametros);
+		JasperPrint impressao = JasperFillManager.fillReport(jr, parametros, new JREmptyDataSource(1));
 		// exibe o resultado
 		JasperViewer viewer = new JasperViewer(impressao, true);
 		viewer.show();
