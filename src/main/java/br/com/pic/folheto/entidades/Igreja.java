042c25e657a55441a013061ed2b71010db0b9509
Ednezer de Godoi Rezende <erezende@brq.com>
2022-03-28 15:25:56 -0300

    AJustes de deploy

--- src/main/java/br/com/pic/folheto/entidades/Igreja.java
+++ src/main/java/br/com/pic/folheto/entidades/Igreja.java
@@ -13,68 +13,64 @@ import java.util.Set;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Igreja {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Builder.Default
 	private Integer id;
 
-	@Builder.Default
 	private String nome;
 
-	@Builder.Default
 	private String cnpj;
 
-	@Builder.Default
 	private String email;
 	
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("membros")
     private List<Membro> membros = new ArrayList<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("pgs")
     private List<PequenoGrupo> pgs = new ArrayList<>();
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("agendas")
     private List<AgendaEvento> agendas = new ArrayList<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
-    private List<Devocional> Devocionais = new ArrayList<>();
+	@Singular("devocionais")
+    private List<Devocional> devocionals = new ArrayList<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("servicos")
     private List<OfertaServico> servicos = new ArrayList<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("mensagens")
     private List<Mensagem> mensagens = new ArrayList<>();
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("missoes")
     private List<Missao> missoes = new ArrayList<>();
 	
 	@OneToOne(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
     private EnderecoIgreja endereco;
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "igreja", cascade=CascadeType.ALL)
-	@Builder.Default
+	@Singular("aniversariantes")
     private List<Aniversariante> aniversariantes = new ArrayList<>();
 
 	@ElementCollection
     @CollectionTable(name = "TELEFONEIGREJA")
-	@Builder.Default
+	@Singular("telefones")
     private Set<String> telefones = new HashSet<>();
 
 }
